6589b4560910b41fe9fd4f7898e7034d910678aa
zaidhusseini <zaidhusseini@gmail.com>
2018-02-09 20:02:15 -0800

    Implement schema file to create 3 tables in chat database

--- server/schema.sql
+++ server/schema.sql
@@ -1,13 +1,34 @@
+
 CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT(10) NOT NULL PRIMARY KEY,
+  room_name VARCHAR(20)
+);
+
+CREATE TABLE users (
+  /* Describe your table here.*/
+  id INT(10) NOT NULL PRIMARY KEY,
+  name VARCHAR(20)
+);
+
+
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT(10) NOT NULL PRIMARY KEY, 
+  message VARCHAR(100), 
+  user_id INTEGER, 
+  room_id INTEGER, 
+  created_at DATE, 
+  updated_at DATE, 
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
 
 
 
